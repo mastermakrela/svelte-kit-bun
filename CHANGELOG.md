a94e9233211f79b9d6a40cad0d80ad7d39cb1de9
mastermakrela <mastermakrela@gmail.com>
2025-02-02 23:25:37 +0100

    refactor: update build process and static file handling; add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.3.0
+
+- Construct static files routes during build process, instead of looking for files on disk
+
+# 0.2.0
+
+- Initial release
--- src/index.js
+++ src/index.js
@@ -1,55 +1,61 @@
-// MARK: - Injected by SvelteKit
+// MARK: - Helpers
+/**
+ *  @param {string} path
+ *  @param {string | undefined} type
+ */
+async function _file(path, type = undefined) {
+	const file = Bun.file(path);
+	if (type && file.type !== type) {
+		console.warn("[svelte-kit-bun]", `Asset ${path} has different type (${file.type}) than declared in SvelteKit (${type})`);
+	}
+	return new Response(await file.bytes(), {
+		headers: {
+			"content-type": type ?? file.type,
+		},
+	});
+}
+
+/**
+ * @param {string} path
+ * @param {string} type
+ */
+async function _immutable(path, type) {
+	const response = await _file(path, type);
+	response.headers.set("cache-control", "public, max-age=31536000, immutable");
+	return response;
+}
+
+/**
+ * @param {string} from
+ * @param {string} to
+ * @param {number} status
+ */
+async function _redirect(from, to, status) {
+	const file = Bun.file(from);
+	return new Response(await file.bytes(), {
+		status,
+		headers: {
+			location: to,
+			"content-type": file.type,
+		},
+	});
+}
+
+// MARK: - Static routes (i.e. prerendered and client files)
+
+// prettier-ignore
+IMPORTS
+
+// prettier-ignore
+/** @type {Record<`/${string}`, Response>} */
+const static_routes = {
+STATIC_ROUTES
+};
+
+// MARK: - Initialize server
 import { Server } from "SERVER";
 
 const manifest = MANIFEST;
-
-/** @type {import('./index.d.ts').Prerendered} */
-const prerendered = PRERENDERED;
-/** @type {string} */
-const app_path = APP_PATH;
-
-// MARK: - Prepare static routes
-const dir = `${import.meta.dir}/client`;
-
-/** @type {Record<string, Response>} */
-const static_routes = {};
-
-for (const [name, { status, location }] of prerendered.redirects) {
-	const file = Bun.file(`${dir}${name}.html`);
-	static_routes[name] = new Response(await file.bytes(), { status, headers: { location, "content-type": file.type } });
-}
-
-for (const [path, { type }] of prerendered.assets) {
-	const file = Bun.file(`${dir}${path}`);
-	if (type !== file.type) {
-		console.warn("[svelte-kit-bun]", `Asset ${path} has different type (${type}) than expected (${file.type})`);
-	}
-	static_routes[path] = new Response(await file.bytes(), { headers: { "content-type": type } });
-}
-
-for (const [path, { file }] of prerendered.pages) {
-	const page = Bun.file(`${dir}/${file}`);
-	static_routes[path] = new Response(await page.bytes(), { headers: { "content-type": page.type } });
-}
-
-const app_path_glob = new Bun.Glob(`${app_path}/**`);
-for await (const pathname of app_path_glob.scan(`${dir}`)) {
-	const file = Bun.file(`${dir}/${pathname}`);
-	const immutable = pathname.startsWith(`${app_path}/immutable/`);
-
-	/** @type {Record<string, string>} */
-	const headers = { "content-type": file.type };
-	if (immutable) headers["cache-control"] = "public, max-age=31536000, immutable";
-
-	static_routes[`/${pathname}`] = new Response(await file.bytes(), { headers });
-}
-
-for (const path of manifest.assets) {
-	const file = Bun.file(`${dir}/${path}`);
-	static_routes[`/${path}`] = new Response(await file.bytes(), { headers: { "content-type": file.type } });
-}
-
-// MARK: - Initialize server
 
 /** @type {import('./index.d.ts').Server} */
 const server = new Server(manifest);
@@ -57,7 +63,14 @@ const server = new Server(manifest);
 await server.init({
 	//@ts-expect-error - Bun.env is NodeJS.ProcessEnv which works, but TS doesn't know that
 	env: Bun.env,
-	read: (file) => Bun.file(`${dir}/${file}`).stream(),
+	// we assume we have all the files, because they are bundled during SVelteKit's compile step
+	read: (file) => {
+		const data = static_routes[`/${file}`].body;
+		if (!data) {
+			throw new Error(`[svelte-kit-bun] File ${file} not found during \`read\` call.`);
+		}
+		return data;
+	},
 });
 
 // MARK: - Serve
